19e8fad493061147543e83e6f758e04dad90ea57
wenke.lai <wenke.studio@gmail.com>
2024-12-11 17:08:01 +0800

    feat: add initial theme and dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,30 @@
 <script lang="ts">
   import "../app.css";
   let { children } = $props();
+  /* 
+  const toggleTheme = (ev: MouseEvent) => {
+    const target = ev.target as HTMLElement;
+    const isDarkMode = target.ownerDocument.documentElement.classList.toggle("dark");
+    // NOT in the iFrame
+    if (target.ownerDocument === document)
+      localStorage.setItem("color-theme", isDarkMode ? "dark" : "light");
+  }; 
+  */
 </script>
+
+<svelte:head>
+  <title>Atrai Go</title>
+  <script>
+    if ("color-theme" in localStorage) {
+      localStorage.getItem("color-theme") === "dark"
+        ? document.documentElement.classList.add("dark")
+        : document.documentElement.classList.remove("dark");
+    } else {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.documentElement.classList.add("dark");
+      }
+    }
+  </script>
+</svelte:head>
 
 {@render children()}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,24 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
+  darkMode: "selector",
+
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))"
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))"
+        }
+      }
+    }
   },
 
   plugins: []
