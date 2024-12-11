05106eb9ec12f01f1028d703997117b04785145f
wenke.lai <wenke.studio@gmail.com>
2024-12-11 17:43:37 +0800

    feat: add an error page for 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    goto("/");
+  });
+</script>
+
+<div class="h-[512px] bg-gray-500/50 p-4">
+  <h1>Oops!</h1>
+</div>
