17fa8a05912593271f40c3869d5cfa595e759155
wenke.lai <wenke.studio@gmail.com>
2024-12-14 22:11:43 +0800

    feat(components): add button component and implement hero section

---
+++ src/lib/components/button/button.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  const { children, onclick } = $props();
+</script>
+
+<button type="button" class="rounded border bg-sky-500 px-4 py-1 active:scale-95" {onclick}>
+  {@render children()}
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,24 @@
-<div class="h-[512px] bg-gray-500/50 p-4">
-  <h1>Welcome to SvelteKit</h1>
+<script>
+  import Button from "$lib/components/button/button.svelte";
+  import { goto } from "$app/navigation";
+</script>
+
+<section class="mx-auto h-[512px] max-w-3xl flex-col p-8">
+  <hgroup class="text-center">
+    <h1>Your Personal</h1>
+    <h2>AI GO Master</h2>
+  </hgroup>
   <p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+    Transform your Go journey with cutting-edge AI tutoring. Whether you're a beginner or an
+    advanced player, our AI mentor adapts to your skill level, learning pace, and goals to create a
+    personalized learning path. Get instant feedback, play matches, and analyze games 24/7 – embark
+    on your unique journey to Go mastery.
   </p>
-</div>
+  <div class="center mt-8 flex gap-8">
+    <Button onclick={() => (window.location.href = "https://google.com")}>Get Started</Button>
+    <Button onclick={() => goto("/playground")}>Demo</Button>
+  </div>
+</section>
 
 <div class="h-[512px] bg-sky-500 p-4">
   <h1>debug</h1>
